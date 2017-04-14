1bc7917e2e483eb34aff1aa45caab186fbe991e1
Alex Dumitru <alexadrian.dmtr@gmail.com>
2017-04-14 20:59:42 +0000

    Make transpiler work

--- index.js
+++ index.js
@@ -2,10 +2,11 @@
 
 var winston = require('winston');
 var _ = require('lodash');
+var fs = require('fs');
 
 class AstNode {
   constructor({type}) {
-    this.type = type;
+    this.type = type
     this.next = null;
     
   }
@@ -28,8 +29,24 @@ class AstNode_Function extends AstNode {
 }
 
 class AstNode_Expression extends AstNode {
-  constructor({type}) {
+  constructor({type, children, operator}) {
     super({type});
+    this.children = children;
+    this.operator = operator;
+  }
+}
+
+class AstNode_Constant extends AstNode {
+  constructor({type, value}) {
+    super({type});
+    this.value = value;
+  }
+}
+
+class AstNode_Variable extends AstNode {
+  constructor({type, value}) {
+    super({type});
+    this.value = value;
   }
 }
 class AstTree {
@@ -38,6 +55,7 @@ class AstTree {
   }
   
   createNode(options) {
+    // console.log(`create ${options.type}`);
     if (options.type === 'block-start')
       return new AstNode(options);
     else if (options.type == 'declare')
@@ -46,8 +64,94 @@ class AstTree {
       return new AstNode_Function(options);
     else if (options.type == 'expression')
       return new AstNode_Expression(options);
+    else if (options.type == 'constant')
+      return new AstNode_Constant(options);
+    else if (options.type == 'variable') {
+      return new AstNode_Variable(options);
+    }
+    else if (options.type == 'end')
+      return new AstNode(options);
+  }
+  
+  // returns a node
+  parseExpression(tokens) {
+    
+    function findOperator(op) {
+      let k = 0;
+      
+      let i = tokens.length-1;
+      while (i >= 0) {
+        if (tokens[i] === ')')
+          k++;
+        else if (tokens[i] === '(')
+          k--;
+        else if (_.some(op.symbols, (symbol) => symbol === tokens[i]) && k === 0)
+          return i;
+      
+        i--;
+      }
+      
+      return -1;
+    }
+    
+    function isLetter(str) {
+     return (str.match(/[a-z]/i)) !== null;
+  }
+    var ops = [
+      {
+        type: 'assign',
+        symbols: ['<-', ':='],
+        priority: 0
+      },
+      {
+        type: 'addition',
+        symbols: ['+'],
+        priority: 1
+      }
+      ];
+      
+    // console.log(tokens)
+    var foundOps = ops.map((op) => {
+      let index = findOperator(op);
+      op.index = index;
+      
+      return op;
+    });
+    
+    foundOps = _.filter(foundOps, (op) => op.index !== -1);
+    foundOps = _.sortBy(foundOps, 'priority');
+    
+    var foundOp = _.head(foundOps);
+    // console.log(foundOp);
+    
+    // console.log(tokens, foundOp == null);
+    if (foundOp == null)
+    {
+        if (!isLetter(tokens[0][0]))
+          return this.createNode({type: 'constant', value: parseFloat(tokens[0])});
+        else
+          return this.createNode({type: 'variable', value: tokens[0]});
+     
+    }
+    else
+    {
+    
+      var left = this.parseExpression(_.slice(tokens, 0, foundOp.index));
+      var right = this.parseExpression(_.slice(tokens, foundOp.index+1));
+      
+      // console.log('left is ', left);
+      // console.log('right is ', right);
+      return this.createNode(
+        {type: 'expression', 
+        children: [left, right],
+        operator: foundOp.type
+      });
+    
+    }
   }
 }
+
+
 
 class Parser {
   static parse(script) {
@@ -78,8 +182,15 @@ class Parser {
         node.next = nextNode;
         node = node.next;
       }
+      else if (tokens[0] == "sfarsit")
+      {
+        let nextNode = tree.createNode({type: 'end'});
+        node.next = nextNode;
+        node = node.next;
+      }
       else {
-        let nextNode = tree.createNode({type: 'expression'});
+        let exprNode = tree.parseExpression(tokens);
+        let nextNode = exprNode;
         node.next = nextNode;
         node = node.next;
       }
@@ -94,20 +205,67 @@ class Parser {
     
     while (node) {
       console.log(_.pickBy(node, (value, key) => key != 'next'));
-      console.log('|')
-      console.log('|')
-      console.log('V')
+      console.log('|');
+      console.log('|');
+      console.log('V');
       node = node.next;
     }
   }
 }
 
-let tree = Parser.parse(`
-  inceput
-  intreg a
-  a <- 2 + 2
-  afisare a
-  sfarsit
-`);
+class Transpiler {
+  static transpileToCpp(tree) {
+    
+    function transpileExpression(node) {
+      if (node.type == 'expression') {
+        if (node.operator == 'assign')
+          return transpileExpression(node.children[0]) + '=' + transpileExpression(node.children[1]);
+        else if (node.operator == 'addition')
+          return transpileExpression(node.children[0]) + '+' + transpileExpression(node.children[1]);
+      }
+      else
+        return node.value;
+    }
+    let code = '';
+    
+    let node = tree.root;
+    
+    while (node) {
+      let line = '';
+      
+      if (node.type == 'declare') {
+        line = node.varType + ' ' + node.registers.join(',') + ';';
+      }
+      else if (node.type == 'expression') {
+        line = transpileExpression(node) + ';';
+      }
+      else if (node.type == 'function') {
+        if (node.functionName == 'print') {
+          line = _.concat('cout', node.parameters, 'endl').join('<<') + ';';
+        }
+      }
+      
+      code = code + line + '\n';
+      
+      node = node.next;
+    }
+    
+    return `#include <iostream>
+    
+    using namespace std;
+    
+    int main()
+    {
+        ${code}
+        
+        return 0;
+    }`;
+  }
+}
 
-Parser.printTree(tree);
+let script = fs.readFileSync('./pseudocod.txt');
+let tree = Parser.parse(script.toString());
+// Parser.printTree(tree);
+// console.log(Transpiler.transpileToCpp(tree));
+
+fs.writeFileSync('./main.cpp', Transpiler.transpileToCpp(tree));
